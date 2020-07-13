9587508f36f1df658e6cc3fefe82fc2a350414d7
Anastasia <anastasia.nesterenko.dev@gmail.com>
2020-07-13 14:46:15 -0400

    Email Verification

--- Identity/Controllers/HomeController.cs
+++ Identity/Controllers/HomeController.cs
@@ -1,10 +1,7 @@
-﻿using Identity.Data;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Security.Claims;
+using NETCore.MailKit.Core;
 using System.Threading.Tasks;
 
 namespace Identity.Controllers
@@ -13,10 +10,13 @@ namespace Identity.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
-        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        private readonly IEmailService _emailService;
+
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IEmailService emailService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _emailService = emailService;
         }
         public IActionResult Index()
         {
@@ -68,20 +68,38 @@ namespace Identity.Controllers
                 UserName = username,
                 Email = "",
             };
+
             var result = await _userManager.CreateAsync(user, password);
 
             if (result.Succeeded)
             {
-                // Sign in user
-                var signInResult = await _signInManager.PasswordSignInAsync(username, password, false, false);
+                // Generation of the email token.
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
-                if (signInResult.Succeeded)
-                {
-                    return RedirectToAction("Index");
-                }
+                var link = Url.Action(nameof(VerifyEmail), "Home", new { userId = user.Id, code }, Request.Scheme, Request.Host.ToString());
+
+                await _emailService.SendAsync("test@test.com", "email verify", $"<a href=\"{link}\">Verify Email</a>", true);
+
+                return RedirectToAction("EmailVerification");
+
             };
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> VerifyEmail(string userId, string code)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return BadRequest();
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+
+            if (result.Succeeded)
+            {
+                return View();
+            }
+
+            return BadRequest();
+        }
+        public IActionResult EmailVerification() => View();
 
         public async Task<IActionResult> LogOut()
         {
@@ -89,6 +107,5 @@ namespace Identity.Controllers
             return RedirectToAction("Index");
 
         }
-
     }
 }
--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -3,13 +3,22 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NETCore.MailKit.Extensions;
+using NETCore.MailKit.Infrastructure.Internal;
 
 namespace Identity
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,6 +36,7 @@ namespace Identity
                     config.Password.RequireDigit = false;
                     config.Password.RequireUppercase = false;
                     config.Password.RequireNonAlphanumeric = false;
+                    config.SignIn.RequireConfirmedEmail = true;
                 })
                 .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
@@ -35,6 +45,11 @@ namespace Identity
             {
                 config.Cookie.Name = "Identity.Cookie";
                 config.LoginPath = "/Home/Login";
+            });
+
+            services.AddMailKit(config =>
+            {
+                config.UseMailKit(_config.GetSection("Email").Get<MailKitOptions>());
             });
 
             services.AddControllersWithViews();
