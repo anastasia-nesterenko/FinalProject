23c6d606c504b1d2fa11eacb60684a9c213021ed
Anastasia <anastasia.nesterenko.dev@gmail.com>
2020-07-20 21:33:38 -0400

    JWT Bearer

---
+++ Server/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Server
+{
+    public class Constants
+    {
+        public const string Issuer = Audience;
+        public const string Audience = "https://localhost:44368/";
+        public const string Secret = "not_too_short_secret_otherwise_it_might_cause_error";
+    }
+}
